f6e880e254ca3a5790404c7fc567d31d4067178d
PeasantTech <94719317+CJArpide@users.noreply.github.com>
2024-12-13 08:58:58 -0500

    Create 5-TransformOverEachElement.js

---
+++ 30DaysOfJS/5-TransformOverEachElement.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let returnArr = [];
+    for (let i = 0; i < arr.length; i++){
+        returnArr[i] = fn(arr[i],i);
+    }
+    return returnArr;
+};
