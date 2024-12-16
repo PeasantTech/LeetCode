a7f21c84faee17a251a5a42b6aa695eee2676157
PeasantTech <94719317+CJArpide@users.noreply.github.com>
2024-12-16 15:31:12 -0500

    Create 3264.FinArrStateAfterKMultipliers.js

---
+++ DailyProblems/JavaScript/3264.FinArrStateAfterKMultipliers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} multiplier
+ * @return {number[]}
+ */
+var getFinalState = function(nums, k, multiplier) {
+    for (let i=1; i <= k; i++){
+        const holdVal = nums.map()
+        nums.map(holdVal) = holdVal * multiplier;
+    }
+    return nums;
+};
