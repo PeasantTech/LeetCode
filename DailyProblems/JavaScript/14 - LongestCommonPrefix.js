2101268befaeab6d78d82835b5d1f034ecdd3b36
PeasantTech <94719317+CJArpide@users.noreply.github.com>
2024-12-18 12:50:21 -0500

    Update 14 - LongestCommonPrefix.js
    finished 14

--- DailyProblems/JavaScript/14 - LongestCommonPrefix.js
+++ DailyProblems/JavaScript/14 - LongestCommonPrefix.js
@@ -6,21 +6,15 @@ var longestCommonPrefix = function (strs) {
     let answerPrefix = "";
     for (let index = 0; index < strs[0].length; index++) {
         let whileLoop = 0;
-        while (whileLoop <= strs[0].length) {
-            console.log("strs[index]: ", strs[index]);
-            console.log("whileLoop: ", whileLoop);
-            console.log("strs[j][index]: ", strs[whileLoop][index]);
+        while (whileLoop <= strs.length) {
             if (strs[whileLoop + 1] === undefined) {
-                console.log("undefined return");
                 break;
             } if (strs[whileLoop][index] !== strs[whileLoop + 1][index]) {
-                // console.log("prefix not equal return");
                 return answerPrefix;
             }
             whileLoop++;
         }
         answerPrefix += strs[whileLoop][index];
-        // console.log("answerPrefix: ", answerPrefix);
     }
     return answerPrefix;
 };
