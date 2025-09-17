93db73867a103162ea606dda44838c29d71f985f
Connor <94719317+PeasantTech@users.noreply.github.com>
2025-09-17 15:41:39 -0400

    Create 2353 - FoodRating.js
    incomplete, leet code September 17th, 2025

---
+++ DailyProblems/JavaScript/2353 - FoodRating.js
@@ -0,0 +1,32 @@
+let frfoods = ["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"];
+let frcuisines = ["korean", "japanese", "japanese", "greek", "japanese", "korean"];
+let frratings = [9, 12, 8, 15, 14, 7];
+
+// let foodRatingsArgs = [frfoods, frcuisines, frratings];
+// const highestRatedArgs1 = ["korean"];
+// const highestRatedArgs2 = ["japanese"];
+// const changeRatingArgs1 = ["sushi", 16];
+// const highestRatedArgs3 = ["japanese"];
+// const changeRatingArgs2 = ["ramen", 16];
+// const highestRatedArgs4 = ["japanese"];
+
+const FoodRatings = (foods, cuisines, ratings) => {
+    const obj = {};
+    for (const key of foods) {
+        obj[key] = {};
+    }
+
+    console.log(obj);
+}
+
+const changeRating = (food, newRating) => {
+
+}
+
+const highestRated = cuisine => {
+
+}
+
+FoodRatings(frfoods, frcuisines, frratings);
+
+
